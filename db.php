54c8ab3f5b79ab2de3857477f88a260e6f9f3b64
Josetamara12 <jose.tamara426@pascualbravo.edu.co>
2026-03-15 14:10:36 -0500

    fix conexion mariadb password root

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = mysqli_connect("mariadb", "root", "", "pascual_bravo");
+
+$host = "mariadb";
+$user = "root";
+$password = "root";
+$db = "pascual_bravo";
+
+$conn = mysqli_connect($host, $user, $password, $db);
 
 if (!$conn) {
     die("Error de conexión: " . mysqli_connect_error());
 }
+
 ?>
